1e81f91f0d7362b91b316634f29b52320ee3840e
TheDreadingDark <31518923+TheDreadingDark@users.noreply.github.com>
2017-10-12 13:11:07 -0600

    Added second screen to storyboard and controller for it.

---
+++ FirstAfternoonDemo/Controller/SecondScreenController.swift
@@ -0,0 +1,14 @@
+//
+//  SecondScreenController.swift
+//  FirstAfternoonDemo
+//
+//  Created by Woolsey, Adam on 10/12/17.
+//
+
+
+import UIKit
+
+public class SecondStringController : UIViewController
+{
+    
+}
