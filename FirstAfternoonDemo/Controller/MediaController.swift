e0f25dfeca424ad1b4b1658b44644edaa315c375
TheDreadingDark <31518923+TheDreadingDark@users.noreply.github.com>
2017-10-23 13:49:38 -0600

    Added images and .mp3 files.

--- FirstAfternoonDemo/Controller/MediaController.swift
+++ FirstAfternoonDemo/Controller/MediaController.swift
@@ -5,17 +5,27 @@
 //  Created by Woolsey, Adam on 10/18/17.
 //
 
+
 import UIKit
+import AVFoundation
 
 public class MediaController : UIViewController
 {
-    
     @IBOutlet weak var imageFrame: UIImageView!
-    @IBOutlet weak var changeImage: UIButton!
-    @IBOutlet weak var playPause: UIButton!
+    @IBOutlet weak var imageButton: UIButton!
+    @IBOutlet weak var soundButton: UIButton!
     @IBOutlet weak var soundSlider: UISlider!
     
+    private lazy var color : ColorTools = ColorTools()
+    private var imageCounter : Int = 0
+    private var soundPlayer : AVAudioPlayer?
     
+    public override func viewDidLoad() -> Void
+    {
+        super.viewDidLoad()
+        view.backgroundColor = color.createRandomColor()
+        loadAudioFile()
+    }
     
     private func changeImage() -> Void
     {
@@ -38,4 +48,26 @@ public class MediaController : UIViewController
         }
         
         imageCounter += 1
+        
+        private func loadAudioFile() -> Void
+        {
+            if let soundURL = NSDataAsset(name: "kogmawjoke")
+            {
+                do
+                {
+                    try! AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+                    try! AVAudioSession.sharedInstance().setActive(true)
+                    
+                    try soundPlayer = AVAudioPlayer(data: soundURL.data, fileTypeHint:
+                        AVFileType.mp3.rawValue)
+                    soundSlider.maximumValue = Float ((soundPlayer?.duration)!)
+//                    Timer.scheduledTimer(timeInterval: 0.2, target: self, selector:
+                        (#selector(self.updateSlider)), userInfo: nil, repeats: true)
+                }
+                catch
+                {
+                    print("Audio File Load Error")
+                }
+            }
+        }
 }
