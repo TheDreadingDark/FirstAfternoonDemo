c3f420a5dad1c48c81c843ba9c95ea6189fc9965
TheDreadingDark <31518923+TheDreadingDark@users.noreply.github.com>
2017-10-24 13:04:56 -0600

    Added code for slider, change image button, and play audio button.

--- FirstAfternoonDemo/Controller/MediaController.swift
+++ FirstAfternoonDemo/Controller/MediaController.swift
@@ -12,8 +12,8 @@ import AVFoundation
 public class MediaController : UIViewController
 {
     @IBOutlet weak var imageFrame: UIImageView!
-    @IBOutlet weak var imageButton: UIButton!
     @IBOutlet weak var soundButton: UIButton!
+    @IBOutlet weak var imageButton: UIButton!
     @IBOutlet weak var soundSlider: UISlider!
     
     private lazy var color : ColorTools = ColorTools()
@@ -25,6 +25,11 @@ public class MediaController : UIViewController
         super.viewDidLoad()
         view.backgroundColor = color.createRandomColor()
         loadAudioFile()
+    }
+    
+    public override func didReceiveMemoryWarning() -> Void
+    {
+        super.didReceiveMemoryWarning()
     }
     
     private func changeImage() -> Void
@@ -42,12 +47,48 @@ public class MediaController : UIViewController
         {
             imageFrame.image = UIImage(named: "tristicon")
         }
-        else
+        else if (imageCounter == 2)
         {
             imageFrame.image = UIImage(named: "zedicon")
         }
         
         imageCounter += 1
+        
+        @IBAction func soundButtonClick() -> Void
+        {
+            playMusicFile()
+            view.backgroundColor = color.createRandomColor()
+        }
+        
+        @IBAction func sliderMethod() -> Void
+        {
+            let seekTime = Double (soundSlider.value)
+            soundPlayer?.currentTime = seekTime
+        }
+        
+        private func playMusicFile() -> Void
+        {
+            if let isPlaying = soundPlayer?.isPlaying
+            {
+                if (isPlaying)
+                {
+                    soundPlayer?.pause()
+                }
+                else
+                {
+                    soundPlayer?.play()
+                }
+            }
+            
+//            if ((soundPlayer?.isPlaying)!)
+//            {
+//                soundPlayer?.pause()
+//            }
+//            else
+//            {
+//                soundPlayer?.play()
+//            }
+        }
         
         private func loadAudioFile() -> Void
         {
@@ -61,7 +102,7 @@ public class MediaController : UIViewController
                     try soundPlayer = AVAudioPlayer(data: soundURL.data, fileTypeHint:
                         AVFileType.mp3.rawValue)
                     soundSlider.maximumValue = Float ((soundPlayer?.duration)!)
-//                    Timer.scheduledTimer(timeInterval: 0.2, target: self, selector:
+                    Timer.scheduledTimer(timeInterval: 0.2, target: self, selector:
                         (#selector(self.updateSlider)), userInfo: nil, repeats: true)
                 }
                 catch
@@ -69,5 +110,10 @@ public class MediaController : UIViewController
                     print("Audio File Load Error")
                 }
             }
+        }
+        
+        @objc private func updateSlider() -> Void
+        {
+            soundSlider.value = Float ((soundPlayer?.currentTime)!)
         }
 }
